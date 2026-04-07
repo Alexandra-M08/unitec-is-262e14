8f3af4273f90b475ac31468c073e0eae8848f68a
Alexandra Martínez Hernandez <xandrabella810@gmail.com>
2026-04-06 23:55:46 -0600

    Agregar proyecto Elipse con tests completos

---
+++ Is262E14Ejercicio03/src/ConsoleApp/Elipse.cs
@@ -0,0 +1,12 @@
+﻿public class Elipse
+{
+    public static double CalcularPerimetro(double a, double b)
+    {
+        return Math.PI * (a + b);
+    }
+
+    public static double CalcularArea(double a, double b)
+    {
+        return Math.PI * a * b;
+    }
+}
